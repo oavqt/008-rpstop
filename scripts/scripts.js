dafa55b0b56a10ec6f01d95ff54a528fb386bd9f
oav <ortizqtv@gmail.com>
2021-06-21 03:56:11 -0500

    le sigh logicalui update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,8 @@
 //
 //rpsp
+
 //-user interface
+
 const btns = document.getElementsByClassName('btn__button');
 
 const btnsArray = [...btns];
@@ -8,6 +10,10 @@ const btnsArray = [...btns];
 const header = document.getElementsByClassName('header')[0];
 
 const footer = document.getElementsByClassName('footer__roundmessage')[0];
+
+const img = document.getElementsByClassName('btn__img');
+
+const imgs = [...img];
 
 header.addEventListener('click', function (e) {
   console.log(e);
@@ -26,10 +32,13 @@ function ol() {
     header.textContent = 'Okay, You Can Stop Now!...';
   }
 }
+
 //-generate player choice
+
 let playerSelection;
 
 //-generate random computer choice
+
 let computerSelection;
 function getRandomChoice(list) {
   return list[Math.floor(Math.random() * list.length)];
@@ -37,36 +46,53 @@ function getRandomChoice(list) {
 function computerPlay() {
   computerSelection = getRandomChoice(['rock', 'paper', 'scissors']);
 }
+
 //-compare player/computer choice and return a value depending on outcome
+
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === 'paper' && computerSelection === 'rock') {
     footer.textContent = 'You Won! Paper beats rock!';
+    playerCount += 1;
   } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
     footer.textContent = 'You Won! Rock beats scissors!';
+    playerCount += 1;
   } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
     footer.textContent = 'You Won! Scissors beats paper!';
+    playerCount += 1;
   } else if (playerSelection === 'rock' && computerSelection === 'paper') {
     footer.textContent = 'You Lose! Paper beats rock!';
+    computerCount += 1;
   } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
     footer.textContent = 'You Loss! Rock beats scissors!';
+    computerCount += 1;
   } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
     footer.textContent = 'You Loss! Scissors beats paper!';
+    computerCount += 1;
   } else {
     footer.textContent = 'You Tied';
   }
 }
 
+//count score and display it to user
+let playerCount = 0;
+let computerCount = 0;
+
+function currentScore() {
+  document.getElementsByClassName(
+    'score'
+  )[0].textContent = `Player = ${playerCount} | ${computerCount} = Doge`;
+}
+
 //play
 btnsArray.forEach((btns) => {
-  btns.addEventListener('click', play);
+  btns.addEventListener('click', runGame);
 });
 
-function play() {
+function runGame() {
   playerSelection = this.value;
   computerPlay();
-  console.log(playerSelection);
-  console.log(computerSelection);
   playRound(playerSelection, computerSelection);
+  currentScore();
 }
 
 // function removeHighlight(e) {
